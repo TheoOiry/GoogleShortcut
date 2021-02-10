9e7355711dd2a8cf3b199af776834e8bfeed393e
Théo Oiry <theo.oiry@giantdev.fr>
2021-02-10 21:35:06 +0100

    remove useless code

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
-    chrome.tabs.executeScript({
-        code: 'window.getSelection().toString();'
-    }, function (selection) {
-        console.log(selection)
-    });
-});
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (tab.url.startsWith("https://www.google.") && tab.url.includes("/search?") && changeInfo.status === "complete") {
         chrome.tabs.executeScript(tabId, {
