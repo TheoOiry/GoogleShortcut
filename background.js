abfe8bbc285e59b9ec05846067a60250cbf7c210
Théo Oiry <theo.oiry@giantdev.fr>
2021-07-15 15:52:32 +0200

    background

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.url.startsWith("https://www.google.") && tab.url.includes("/search?") && changeInfo.status === "complete") {
+    if (tab.url && tab.url.startsWith("https://www.google.") && tab.url.includes("/search?") && changeInfo.status === "complete") {
         chrome.tabs.executeScript(tabId, {
             file: 'inject.js'
         });
